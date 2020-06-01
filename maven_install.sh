79e48bd43adf296098b9758be798bd74d576a4b9
witkey9876 <witkey9876@qq.com>
2020-06-01 11:12:17 +0800

    update fuse version

---
+++ maven_install.sh
@@ -0,0 +1,2 @@
+# maven install path
+tools/apache-maven-3.5.0/bin/mvn clean install -Dmaven.repo.local=tools/local_repo/ -Dsource=8
---
+++ readme.md
@@ -0,0 +1,5 @@
+## maven相关
+1. tools包含maven lib
+2. 修改setting.xml指定jar安装目录
+3. 执行maven_install.sh 可下载依赖到指定目录(可修改)
+
